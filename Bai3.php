2f52f6ece88453f965c56dc2c6519b432341a4ad
nguyenlinh01 <lepetitprince.cz@gmail.com>
2018-03-27 19:38:35 +0700

    Add files via upload

--- Bai3.php
+++ Bai3.php
@@ -1,13 +1,9 @@
-
-<form method="POST">
-    Name: <input type="text" name="name" value="<?php if(isset($_POST['name'])){echo $_POST['name'];}?>"/><br/><br/>
-    Email: <input type="text" name="email" value="<?php if(isset ($_POST['email'])){echo $_POST['email'];} ?>"/><br/><br/>
-    Website: <input type="text" name="website" value="<?php if(isset ($_POST['website'])){echo $_POST['website'];} ?>"/><br/><br/>
-    Comment: <input type="text" name="comment" value="<?php if(isset ($_POST['comment'])){echo $_POST['comment'];} ?>"/><br/><br/>
-    Gender: <input type="radio" name="gender" value=""/>Female
-            <input type="radio" name="gender" value=""/>Male
-            <input type="submit"/>
+<form method="GET">
+	<p>Name: <input type="text" name="inputName" value="<?php if(isset($_GET['inputName'])){echo $_GET['inputName'];} ?>"/></p>
+	<p>Email: <input type="text" name="inputEmail" value="<?php if(isset($_GET['inputEmail'])){echo $_GET['inputEmail'];}?>"></p>
+	<p>Websides: <input type="text" name="inputWeb" value="<?php if(isset($_GET['inputWeb'])){echo $_GET['inputWeb'];}?>"></p>
+	<p>Comment: <textarea name="inputComment"><?php if(isset($_GET['inputComment'])){echo htmlentities ($_GET['inputComment']);}?></textarea></p>
+	<p>Gender: <input type="radio" name="Gender" <?php if(isset($_GET['Gender']) && $_GET['Gender'] == "Male"){echo "checked";}?> value="Male">Male
+				<input type="radio" name="Gender" <?php if(isset($_GET['Gender']) && $_GET['Gender'] == "Female"){echo "checked";}?> value="Female">Female</p>
+	<input type="submit" name="">
 </form>
-
-
-
